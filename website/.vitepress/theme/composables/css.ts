54aacc4c265e1153e7d0ada96ffb20dcc6737914
qind <qindlute@gmail.com>
2026-04-13 22:34:31 +0800

    update

--- website/.vitepress/theme/composables/css.ts
+++ website/.vitepress/theme/composables/css.ts
@@ -2,53 +2,56 @@
 import type { navItem } from './types'
 
 const css: navItem[] = [
-  {
+    {
     id: 1,
-    text: 'colorhunt',
-    link: 'https://colorhunt.co/',
-    desc: 'Discover the newest hand-picked palettes of Color Hunt',
-  },
-  {
-    id: 2,
-    text: 'colordrop',
-    link: 'https://colordrop.io/',
-    desc: 'Color drop list',
-  },
-  {
-    id: 3,
-    text: 'Material Design Color',
-    link: 'https://www.materialpalette.com/',
-    desc: 'Material design palette',
-  },
-  {
-    id: 4,
-    text: 'The color of anything',
-    link: 'https://picular.co/',
-    desc: 'Match colors based on search content',
-  },
-  {
-    id: 5,
     text: '中国色',
     link: 'http://zhongguose.com/',
     desc: 'Chinese colors',
   },
   {
-    id: 6,
+    id: 2,
     text: '渐变配色',
     link: 'http://color.oulu.me/',
     desc: 'A collection of 180 free linear gradients for websites',
   },
   {
-    id: 7,
+    id: 3,
+    text: 'colorhunt',
+    link: 'https://colorhunt.co/',
+    desc: 'Discover the newest hand-picked palettes of Color Hunt',
+  },
+  {
+    id: 4,
+    text: 'colordrop',
+    link: 'https://colordrop.io/',
+    desc: 'Color drop list',
+  },
+
+  {
+    id: 5,
+    text: 'The color of anything',
+    link: 'https://picular.co/',
+    desc: 'Match colors based on search content',
+  },
+
+  {
+    id: 6,
     text: 'CSS Easing Animation Tool',
     link: 'https://matthewlein.com/tools/ceaser',
     desc: 'Choose an easing type and test it out with a few effects',
   },
   {
-    id: 8,
+    id: 7,
     text: 'CSS Support Guide for Email',
     link: 'https://www.campaignmonitor.com/css/',
     desc: 'A complete breakdown of the CSS support for the most popular email clients',
+  },
+
+  {
+    id: 8,
+    text: 'Qindlute Advertise Html',
+    link: 'https://docs.qindlute.cloud/advertise.html/',
+    desc: 'qindlute advertise',
   },
 ]
 
