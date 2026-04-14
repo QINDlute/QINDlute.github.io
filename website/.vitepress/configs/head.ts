30d7c17a88037706217b7255918f56bce90d7021
qind <qindlute@gmail.com>
2026-04-14 11:51:14 +0800

    font awesome库

--- website/.vitepress/configs/head.ts
+++ website/.vitepress/configs/head.ts
@@ -3,6 +3,8 @@ import type { HeadConfig } from 'vitepress'
 export const head: HeadConfig[] = [
     // 配置网页图标
     ['link', { rel: 'icon', type: 'image/x-icon', href: '/img/qind_ico.svg' }],
+    // 引入 Font Awesome 图标库
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' }],
     // 引入外部脚本文件
     ['script', { src: '/js/early-init.js' }],
     ['script', { src: '/js/image-protection.js' }],
