95d819ef2e99a12ededb1441a3a486e7c6e5eeee
qind <qindlute@gmail.com>
2026-05-17 01:21:42 +0800

    update

--- website/.vitepress/configs/sidebar.ts
+++ website/.vitepress/configs/sidebar.ts
@@ -68,6 +68,7 @@ export const sidebar: DefaultTheme.Config["sidebar"] = {
                     items: [
                         { text: '定义', link: '/study/math/4_不定积分/1-定义' },
                         { text: '工具', link: '/study/math/4_不定积分/2-不定积分工具' },
+                        { text: '有理函数不定积分', link: '/study/math/4_不定积分/3-有理函数不定积分' },
                         { text: '指数函数导数证明', link: '/study/math/4_不定积分/指数函数导数证明' },
                     ]
                 },
